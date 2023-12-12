b18f4fcd5e2d4bade2398ada69194fc0743fc5c0
ADOX16 <153356508+ADOX16@users.noreply.github.com>
2023-12-12 15:52:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+var root = document.querySelector(':root');
+var rootStyles = getComputedStyle(root);
+let main = document.getElementById('#main');
+
+let hndrd1 = toString(Math.floor(Math.random() * 100) +1) + '%';
+let hndrd2 = toString(Math.floor(Math.random() * 100) +1) + '%';
+let hndrd3 = toString(Math.floor(Math.random() * 100) +1) + '%';
+let hndrd4 = toString(Math.floor(Math.random() * 100) +1) + '%';
+let hndrd5 = toString(Math.floor(Math.random() * 100) +1) + '%';
+let hndrd6 = toString(Math.floor(Math.random() * 100) +1) + '%';
+//main.style.background = `conic-gradient(at ${hndrd1},${hndrd2} rgb(107, 0, 194) , rgb(58, 0, 194) , rgb(4, 0, 255)) padding-box;`
+
+
+
+//window.addEventListener('mousemove', function(e){});
+*/
+
